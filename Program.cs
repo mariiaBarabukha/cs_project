65669ad71917b9c0875c288bb120503ada69d27a
ari <mariia.barabukha@ukma.edu.ua>
2021-02-25 13:16:59 +0200

    all classes added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var costumer = new Costumer("Vasya", "Volk", "gfvolkvasya@gmail.com");
+            costumer.AddCategory("c1");
+            costumer.AddCategory("c2");
+
+            costumer.AddWallet("wallet1", 2, "useless wallet", "ukr grivna");
+
+
+            var costumer2 = new Costumer("Ari", "Aru", "mariia.barabukha@gmail.com");
+            costumer.ShareWallet(costumer2, costumer.GetWallets()[0]);
+
+            costumer.GetWallets()[0].MakeTransaction(100, "ukr grivna", costumer.GetCategories()[0], "test", DateTime.Now);
+           // Console.WriteLine(costumer.GetWallets()[0].transactions[0].Show()); 
+            costumer.GetWallets()[0].ShowTransactions();
         }
     }
 }
